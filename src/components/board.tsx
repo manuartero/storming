7c5b14e60ce8cfac111cb98419fc30b9481c4770
Manu Artero Anguita <manutero.developer@gmail.com>
2022-07-31 18:31:32 +0200

    feat: basic ' Board - Cell - Hexagon ' structure

---
+++ src/components/board.tsx
@@ -0,0 +1,82 @@
+import "./board.scss";
+import Cell from "./cell";
+
+interface Props {
+  foo?: string;
+}
+
+/**
+ * 5+6+7+8+7+6+5
+ */
+function Board({ foo }: Props): JSX.Element {
+  return (
+    <div className="board">
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+      <Cell />
+
+    </div>
+  );
+}
+
+export default Board;
