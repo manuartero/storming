c3e2df7b845b7a861174effd1cf8e07403bcf2a5
Manu Artero Anguita <manutero.developer@gmail.com>
2022-11-01 12:49:13 +0100

    review grid css.
     - using fixed board height (?)
     - review grid layout
     - remove unnecessary statements

--- src/components/cards/hand.tsx
+++ src/components/cards/hand.tsx
@@ -1,19 +1,18 @@
-import Card from './card';
+import IconCard from "./icon-card";
+import "./hand.scss";
 
-import './hand.scss';
-
-interface Props {
-  foo?: string;
-}
-
-function Hand({ foo }: Props): JSX.Element {
-  return <div className='player-hand'>
-    <Card action='build' />
-    <Card action='diplo' />
-    <Card action='move' />
-    <Card action='move' />
-    <Card action='recruit' />
-  </div>;
+function Hand(): JSX.Element {
+  return (
+    <div className="player-hand">
+      <IconCard
+        card={{
+          cardType: "actionCard",
+          owner: "player",
+          action: "diplo",
+        }}
+      />
+    </div>
+  );
 }
 
 export default Hand;
